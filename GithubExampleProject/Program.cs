e68ae538b2f8d1eca5dd4ab42226f954fb1a2068
Shivaram008 <ram.cheruku@cognine.com>
2021-05-07 14:20:18 +0530

    ExampleAdd project files.

--- GithubExampleProject/Program.cs
+++ GithubExampleProject/Program.cs
@@ -14,6 +14,7 @@ namespace GithubExampleProject
             for(int i=0;i<4;i++)
             {
                 Console.Write(i + " ");
+                Console.Read();
             }
         }
     }
