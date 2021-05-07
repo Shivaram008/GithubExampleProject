e7f599c8786d1fae9fb3778d7d4bc996a6ac26d8
Shivaram008 <ram.cheruku@cognine.com>
2021-05-07 13:19:45 +0530

    Add project files.

---
+++ GithubExampleProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubExampleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            for(int i=0;i<4;i++)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
